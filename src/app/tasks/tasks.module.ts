d760f2ca2e8b2dc3d1568b12eb77f6f26f73351f
irinapizane <pizans1@gmail.com>
2023-01-11 13:37:45 +0200

    Structure update: created shared, material, tasks modules

---
+++ src/app/tasks/tasks.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryComponent } from './components/category/category.component';
+
+@NgModule({
+  declarations: [CategoryComponent],
+  imports: [CommonModule],
+})
+export class TasksModule {}
