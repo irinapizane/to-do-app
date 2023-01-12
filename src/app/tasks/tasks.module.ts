b2660ad8af9fe7ddfe794feba81620892239adce
irinapizane <pizans1@gmail.com>
2023-01-12 10:58:54 +0200

    created single category component

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './components/category/category.component';
 import { SharedModule } from '../shared/shared.module';
 import { CategoryListComponent } from './components/category-list/category-list.component';
+import { SingleCategoryComponent } from './components/single-category/single-category.component';
 
 @NgModule({
-  declarations: [CategoryComponent, CategoryListComponent],
+  declarations: [
+    CategoryComponent,
+    CategoryListComponent,
+    SingleCategoryComponent,
+  ],
   imports: [CommonModule, SharedModule],
-  exports: [CategoryComponent, CategoryListComponent]
+  exports: [CategoryComponent, CategoryListComponent],
 })
 export class TasksModule {}
