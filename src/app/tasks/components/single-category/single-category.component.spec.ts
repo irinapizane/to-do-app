77855daba6ac15fdc4c7c4d54182c2fdc17f242c
irinapizane <pizans1@gmail.com>
2023-01-13 13:51:44 +0200

    injected activatedroute test

--- src/app/tasks/components/single-category/single-category.component.spec.ts
+++ src/app/tasks/components/single-category/single-category.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { SingleCategoryComponent } from './single-category.component';
@@ -9,10 +10,9 @@ describe('SingleCategoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SingleCategoryComponent ],
-      imports: [RouterTestingModule]
-    })
-    .compileComponents();
+      declarations: [SingleCategoryComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SingleCategoryComponent);
     component = fixture.componentInstance;
@@ -21,5 +21,11 @@ describe('SingleCategoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an injected ActivatedRoute', () => {
+    const fixture = TestBed.createComponent(SingleCategoryComponent);
+    const component = fixture.componentInstance;
+    expect(component['route'] instanceof ActivatedRoute).toBeTruthy();
   });
 });
