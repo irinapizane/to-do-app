b99ae44bde686562653835ac37bd4552d60d9710
irinapizane <pizans1@gmail.com>
2023-01-26 15:22:28 +0200

    conflicts resolved

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -5,18 +5,19 @@ import { SharedModule } from '../shared/shared.module';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { SingleCategoryComponent } from './components/single-category/single-category.component';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskComponent, SharedModule_1 as SharedModule } from '../shared/components/task/task.component';
 
 const routes: Routes = [
   { path: 'category/:title', component: SingleCategoryComponent },
 ];
 
 @NgModule({
-  declarations: [
-    CategoryComponent,
-    CategoryListComponent,
-    SingleCategoryComponent,
-  ],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
-  exports: [CategoryComponent, CategoryListComponent],
+    declarations: [
+        CategoryComponent,
+        CategoryListComponent,
+        SingleCategoryComponent,
+    ],
+    exports: [CategoryComponent, CategoryListComponent],
+    imports: [CommonModule, SharedModule, RouterModule.forChild(routes), SharedModule_1]
 })
 export class TasksModule {}
